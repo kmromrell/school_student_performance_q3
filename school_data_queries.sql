f7aca98b2298c63faf79e14f38aa7bb6f9b53d73
Katie Romrell <kmromrell@gmail.com>
2025-05-25 23:42:55 -0700

    Basic descriptive stats to understand spread of data

---
+++ school_data_queries.sql
@@ -0,0 +1,44 @@
+-- What are the percentages at which each grade point is received (e.g., 3, not 3.3)
+SELECT
+	grade_point_used,
+	count(student_id) AS count,
+	round(count(student_id)/(
+		SELECT count(g.student_id)
+		FROM grades AS g 
+	)*100, 2) AS perc
+FROM grades
+GROUP BY 
+	grade_point_used
+ORDER BY grade_point_used DESC;
+
+
+-- How many As, Bs, Cs, Ds, and Fs are given out in core subjects
+SELECT
+	grade_point_used,
+	count(student_id) AS count,
+	round(count(student_id)/(
+		SELECT count(g.student_id)
+		FROM grades AS g 
+		WHERE course_subject IN ('English', 'Science', 'Mathematics', 'Social Studies')
+	)*100, 2) AS perc
+FROM grades
+WHERE course_subject IN ('English', 'Science', 'Mathematics', 'Social Studies')
+GROUP BY 
+	grade_point_used
+ORDER BY grade_point_used DESC;
+
+-- How many As, Bs, Cs, Ds, and Fs are given out in non-core subjects
+SELECT
+	grade_point_used,
+	count(student_id) AS count,
+	round(count(student_id)/(
+		SELECT count(g.student_id)
+		FROM grades AS g 
+		WHERE course_subject NOT IN ('English', 'Science', 'Mathematics', 'Social Studies')
+	)*100, 2) AS perc
+FROM grades
+WHERE course_subject NOT IN ('English', 'Science', 'Mathematics', 'Social Studies')
+GROUP BY 
+	grade_point_used
+ORDER BY grade_point_used DESC;
+
