0acc3e891964d200b8ec46c7ca7d26604903cdf1
Katie Romrell <kmromrell@gmail.com>
2025-05-26 16:50:20 -0700

    Update org/queries -- create table for R analysis

--- school_student_performance_q3/school_data_queries.sql
+++ school_student_performance_q3/school_data_queries.sql
@@ -221,3 +221,67 @@ GROUP BY course_title, teacher_id
 ORDER BY average DESC;
 
 
+
+
+
+
+
+-- Create table with all relevant information for R analysis
+
+CREATE TABLE all_student_data AS ( 
+	SELECT
+		g.student_id,
+		g.grade,
+		g.grade_point_dec,
+		g.grade_point_used,
+		g.course_subject,
+		g.course_title,
+		
+		-- Retrieve absences by period; default to 0 to account for students with no absences
+		COALESCE(CASE g.period
+			    WHEN 1 THEN a.period_1
+	   			WHEN 2 THEN a.period_2
+				WHEN 3 THEN a.period_3
+	    		WHEN 4 THEN a.period_4
+	    		WHEN 5 THEN a.period_5
+	    		WHEN 6 THEN a.period_6 
+	    		WHEN 7 THEN a.period_7
+	    		WHEN 8 THEN a.period_8
+	    		ELSE NULL
+	  	END, 0) AS absences,
+	  		  	
+	  	-- Retrieve tardies by period; default to 0 to account for students with no tardies
+	  	COALESCE(CASE g.period
+			    WHEN 1 THEN tar.period_1 
+	   			WHEN 2 THEN tar.period_2
+				WHEN 3 THEN tar.period_3
+	    		WHEN 4 THEN tar.period_4
+	    		WHEN 5 THEN tar.period_5
+	    		WHEN 6 THEN tar.period_6 
+	    		WHEN 7 THEN tar.period_7
+	    		WHEN 8 THEN tar.period_8
+	    		ELSE 0
+	  	END, 0) AS tardies,
+	  	COALESCE(a.support_seminar, 0) AS ss_absences,
+	  	gpa.gpa,
+	  	gpa.credits_attempted,
+	  	gpa.credits_completed,
+	  	s.gender,
+	  	s.grade_level,
+	  	
+	  	-- Demographic fields; default to 0 for Boolean logic (if unlisted, not part of program)
+	  	COALESCE(e.ell, 0) AS ell,
+	    COALESCE(s504.sec_504, 0) AS sec_504,
+	    COALESCE(sped.sped, 0) AS sped,
+	    COALESCE(tag.tag, 0) AS tag
+	FROM grades AS g
+	LEFT JOIN absences AS a USING(student_id)
+	LEFT JOIN tardies as tar USING(student_id)
+	LEFT JOIN ell as e USING(student_id)
+	LEFT JOIN gpa USING(student_id)
+	LEFT JOIN sec_504 as s504 USING(student_id)
+	LEFT JOIN sped USING(student_id)
+	LEFT JOIN students as s USING(student_id)
+	LEFT JOIN tag as tag USING(student_id)
+	LEFT JOIN transfer as tr USING(student_id)
+);
