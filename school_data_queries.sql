d07bf8d5403032cc86864c687a6aba59ec8b402b
Katie Romrell <kmromrell@gmail.com>
2025-05-25 23:43:55 -0700

    Break down grades by department

--- school_data_queries.sql
+++ school_data_queries.sql
@@ -42,3 +42,25 @@ GROUP BY
 	grade_point_used
 ORDER BY grade_point_used DESC;
 
+-- All department average grades
+SELECT 
+	round(avg(grade_point_dec), 2) AS average,
+	course_subject
+FROM grades
+GROUP BY course_subject
+ORDER BY average DESC;
+
+
+
+-- English department analytics (looking only at core English classes)
+
+-- English department average grade by teacher
+
+SELECT 
+	round(avg(grade_point_dec), 2) AS average,
+	teacher_id
+FROM grades
+WHERE course_subject='English'
+GROUP BY teacher_id
+ORDER BY average DESC;
+
